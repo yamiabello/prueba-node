76fe131e086205e7f1abfa641a1095178ceadb8b
yamiabello <yamiabello@gmail.com>
2021-12-18 19:33:58 -0500

    Agregue un comentario

--- Clase4_Calculadora/calculadora.js
+++ Clase4_Calculadora/calculadora.js
@@ -4,9 +4,10 @@ const multiplicar = require ('./multiplicar');
 const dividir = require ('./dividir');
 
 
-console.log(sumar(15,15));
+console.log(sumar(15,15));// esto suma
 console.log(restar(50,15));
 console.log(multiplicar(0,9));
 console.log(dividir(0,10));
 
+// Hola Arequipe
 
